0bab1c9d899c817f70860870549139485bd94cb6
AbhayPotle <abhaypotle@users.noreply.github.com>
2026-06-17 10:55:22 +0530

    style: upgrade global layout background with animated neon gradient blobs

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -28,10 +28,19 @@ export default function RootLayout({
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
       </head>
-      <body className="min-h-full flex flex-col bg-cyber-black text-gray-100 antialiased relative selection:bg-neon-cyan selection:text-black">
+      <body className="min-h-full flex flex-col bg-cyber-black text-gray-100 antialiased relative selection:bg-neon-cyan selection:text-black overflow-x-hidden">
         {/* Background Grids */}
         <div className="fixed inset-0 cyber-grid -z-20 pointer-events-none" />
         <div className="fixed inset-0 cyber-grid-radial -z-10 pointer-events-none" />
+        
+        {/* Animated glowing mesh blobs */}
+        <div className="fixed inset-0 overflow-hidden -z-15 pointer-events-none">
+          <div className="neon-blob blob-cyan -top-[10%] -left-[10%]" />
+          <div className="neon-blob blob-magenta -bottom-[15%] -right-[10%]" />
+          <div className="neon-blob blob-yellow top-[20%] right-[10%]" />
+          <div className="neon-blob blob-purple bottom-[30%] left-[20%]" />
+        </div>
+
         {/* CRT Scanline effect */}
         <div className="scanline" />
         
